d9c760dae16e0e99f8c49e75a14d2b27ea5dbd95
msalvati1997 <salvati.martina@outlook.it>
2022-10-07 12:22:08 +0200

    add hosts sensors

--- config/hwpc-sensor/start.sh
+++ config/hwpc-sensor/start.sh
@@ -1,1 +1,1 @@
-./hwpc-sensor --config-file config_file.json
+docker run --rm --net=host --privileged --pid=host -v /sys:/sys -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /tmp/powerapi-sensor-reporting:/reporting -v $(pwd):/srv -v $(pwd)/config_file.json:/config_file.json powerapi/hwpc-sensor --config-file /config_file.json 
