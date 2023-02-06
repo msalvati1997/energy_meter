f7067691c28297dd5a2db1422026f5aa1b2ac498
Msalvati <Msalvati@localhost>
2023-02-06 04:21:40 -0500

    message

--- test.sh
+++ test.sh
@@ -3,7 +3,5 @@ sudo ./host_sensors/auto_config_smartwatts.sh
 sudo docker compose -f host_sensors/docker-compose.yaml down
 sudo docker compose -f test/docker-compose.yaml down
 sudo docker compose -f host_sensors/docker-compose.yaml up -d
-sleep 15
-sudo docker compose -f test/docker-compose.yaml up -d
 sleep 60
-python ./test/main.py
+sudo docker compose -f test/docker-compose.yaml up 
