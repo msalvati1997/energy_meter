0e81cd8adbec2d95d61c049ae8429264843276ad
msalvati1997 <salvati.martina@outlook.it>
2023-02-06 13:56:48 +0100

    update test

--- test.sh
+++ test.sh
@@ -2,6 +2,4 @@
 #sudo ./host_sensors/auto_config_smartwatts.sh
 sudo docker compose -f host_sensors/docker-compose.yaml down
 sudo docker compose -f test/docker-compose.yaml down
-sudo docker compose -f host_sensors/docker-compose.yaml up -d
-sleep 60
-sudo docker compose -f test/docker-compose.yaml up 
+sudo docker compose -f host_sensors/docker-compose.yaml up 
